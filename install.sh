1024deaa6f26c736563ddb8aa3ac6ae50897b723
DanielSchuette <d.schuette@online.de>
2019-03-05 23:16:33 +0100

    add .gitignore, install.sh with --cover

--- install.sh
+++ install.sh
@@ -4,3 +4,10 @@
 for file in main/*.go; do
     go run "$file" || exit 1;
 done
+
+# determine test coverage with --cover
+# (meant for local usage)
+if [[ $1 == "--cover" ]]; then
+    go test . -v -cover -race -coverprofile=coverage.txt -covermode=atomic
+    go tool cover -html=coverage.txt
+fi
